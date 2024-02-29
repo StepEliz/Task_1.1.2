9beaefdecc8d55d20be917164e9fcf4ca4180d6a
Степанова_Елизавета <stepanowa.liza2001@gmail.com>
2024-02-29 19:12:26 +0300

    Реализована работа с БД SQL

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    static Connection connection;
+
+    public Util() throws SQLException {
+
+        final String URL = "jdbc:mysql://localhost:3306/mydb";
+        final String USER = "root";
+        final String PASSWORD = "root";
+
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Не удалось зарегестрировать драйвер");
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
 }
